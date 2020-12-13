dd234d3f57b944dc41c3cd5245a3c5a5c795d8bc
Yadunandan Batchu <nandubatchu@gmail.com>
2020-12-13 18:02:14 +0530

    add electron-builder

--- main.js
+++ main.js
@@ -26,6 +26,10 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 }
 
+app.on("ready", () => {
+	autoUpdater.checkForUpdatesAndNotify();
+});
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
